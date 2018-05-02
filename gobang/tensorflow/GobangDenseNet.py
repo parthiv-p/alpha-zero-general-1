9f2eaf0ee8db0d318e89d6c07bab337f06037ed5
Parthiv P <parthiv.parikh95@gmail.com>
2018-05-01 20:35:22 -0400

    Fixed Compilation error. Architecture updated

--- gobang/tensorflow/GobangDenseNet.py
+++ gobang/tensorflow/GobangDenseNet.py
@@ -4,6 +4,7 @@ from utils import *
 
 import tensorflow as tf
 
+
 class GobangNNet():
     def __init__(self, game, args):
         # game params
@@ -27,23 +28,23 @@ class GobangNNet():
 
             x_image = tf.reshape(self.input_boards, [-1, self.board_x, self.board_y, 1])                    # batch_size  x board_x x board_y x 1
             
-            x1 = tf.convert_to_tensor(x_image, np.float32)
+        
             # conv layer 1
-            output= tf.layers.conv2d(inputs= x1,
+            output= tf.layers.conv2d(inputs= x_image,
                                         filters=24,
                                         kernel_size=[1,1],
                                        padding="same",
                                        activation=tf.nn.relu)
             numblocks= 3
             for i in range(numblocks-1):
-                output= dense_block(output)
-                output= transition_layer(output)
+                output= self.dense_block(output)
+                output= self.transition_layer(output)
 
-            dense= dense_block(output)
-            pool1= tf.layers.max_pooling2d(inputs= dense,pool_size=[2,2], strides=2)
+            dense= self.dense_block(output)
+            
 
             # fully connected layers
-            h_conv4_flat= tf.reshape(pool1, [-1,int(pool1.shape[-1])*int(pool1.shape[-2])*int(pool1.shape[-3])])
+            h_conv4_flat= tf.layers.flatten(dense)
             
 
             s_fc1 = Dropout(Relu(BatchNormalization(Dense(h_conv4_flat, 1024), axis=1, training=self.isTraining)), rate=self.dropout) # batch_size x 1024
@@ -58,70 +59,70 @@ class GobangNNet():
       return tf.layers.conv2d(x, out_channels, kernel_size=[3,3], padding=padding)
 
     
-    def dense_block(features):
+    def dense_block(self, features):
 
         #convolutional layer 2
-        conv1= features
-        conv2= tf.layers.conv2d(inputs= conv1,
+        _input = features
+        batchnorm1 = tf.layers.batch_normalization(features)
+        relu1 = tf.nn.relu(batchnorm1)
+
+        conv1= tf.layers.conv2d(inputs= relu1,
+                                filters=12*4,
+                                kernel_size=[1,1],
+                               padding="same")
+
+        
+        batchnorm2 = tf.layers.batch_normalization(conv1)
+        relu2 = tf.nn.relu(batchnorm2)
+
+        conv2= tf.layers.conv2d(inputs= relu2,
                                 filters=12,
                                 kernel_size=[3,3],
-                               padding="same",
-                               activation=tf.nn.relu)
+                               padding="same")
 
-        #concatenation   
-        concat1= tf.concat([conv1,conv2],-1)
+        concat1 = tf.concat([_input, conv2],-1)
 
-        # conv layer 3
+        batchnorm1b = tf.layers.batch_normalization(concat1)
+        relu1b = tf.nn.relu(batchnorm1b)
 
-        conv3a= tf.layers.conv2d(inputs= conv2, filters=12*4, kernel_size=[1,1],
-                             padding="same", activation=tf.nn.relu)
+        conv1b = tf.layers.conv2d(inputs= relu1b,
+                                filters=12*4,
+                                kernel_size=[1,1],
+                               padding="same")
 
-        conv3= tf.layers.conv2d(inputs= conv3a, filters=12, kernel_size=[3,3],
-                             padding="same", activation=tf.nn.relu)
+        
+        batchnorm2b  = tf.layers.batch_normalization(conv1b)
+        relu2b  = tf.nn.relu(batchnorm2b)
 
-        #concatenation
-        concat2= tf.concat([concat1, conv3], -1) 
+        conv2b = tf.layers.conv2d(inputs= relu2b,
+                                filters=12,
+                                kernel_size=[3,3],
+                               padding="same")
 
-        # conv layer 3
+        concat2 = tf.concat([concat1, conv2b],-1)
+        
+        return concat2
 
-        conv4a= tf.layers.conv2d(inputs= concat2, filters=12*4, kernel_size=[1,1],
-                             padding="same", activation=tf.nn.relu)
-
-        conv4= tf.layers.conv2d(inputs= conv4a, filters=12, kernel_size=[3,3],
-                             padding="same", activation=tf.nn.relu)
-
-        #concatenation
-        concat3= tf.concat([concat2, conv4], -1) 
-        return concat3
-
-
-
-    def transition_layer(features):
+    def transition_layer(self, features):
             # conv layer 1
 
         conv1= tf.layers.conv2d(inputs= features,
                                 filters=int(features.shape[-1]),
-                                kernel_size=[5,5],
+                                kernel_size=[1,1],
                                padding="same",
                                activation=tf.nn.relu)
 
-        pool1= tf.layers.max_pooling2d(inputs= conv1, pool_size=[2,2], strides=2)
+        #pool1= tf.layers.max_pooling2d(inputs= conv1, pool_size=[2,2], strides=2)
 
-        return pool1
+        return conv1
     
-   
-
 
     def calculate_loss(self):
         self.target_pis = tf.placeholder(tf.float32, shape=[None, self.action_size])
         self.target_vs = tf.placeholder(tf.float32, shape=[None])
-        self.loss_pi =  tf.losses.softmax_cross_entropy_with_logits(self.target_pis, self.pi)
+        self.loss_pi =  tf.losses.softmax_cross_entropy(self.target_pis, self.pi)
         self.loss_v = tf.losses.mean_squared_error(self.target_vs, tf.reshape(self.v, shape=[-1,]))
         self.total_loss = self.loss_pi + self.loss_v
         update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         with tf.control_dependencies(update_ops):
             self.train_step = tf.train.AdamOptimizer(self.args.lr).minimize(self.total_loss)
-
-
-
-
